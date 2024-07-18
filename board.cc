715c5200c1f296015ee0b5d591adfb496aec016b
Dinhbaon <dinhbao142@gmail.com>
2024-07-17 22:05:47 -0400

    created allPieces methods on board

--- board.cc
+++ board.cc
@@ -1,7 +1,12 @@
 #include "board.h"
 #include "move.h"
+#include "piece.h"
+#include <vector>
 
 Square* Board::getSquare(const int x, const int y) const {
+    if (xDimension * y +x > 63 || xDimension * y +x < 0) {
+        return nullptr; 
+    }
     return board[xDimension*y + x];
 }
 
@@ -12,5 +17,25 @@ void Board::movePiece(const int fromX, const int fromY, const int toX, const int
         this->getSquare(fromX, fromY)->setPiece(nullptr);
         this->getSquare(toX, toY)->setPiece(piece);
     } 
+    updateAllPieces(); 
+}
+
+Board::Board() {
+    updateAllPieces(); 
+} 
+
+void Board::updateAllPieces() {
+    std::vector<Piece> blackPieces; 
+    std::vector<Piece> whitePieces; 
+
+    for (Square* square: this->board) {
+        if (square->getPiece()->getIsWhite() == true) whitePieces.emplace_back(square->getPiece()); 
+
+        if (square->getPiece()->getIsWhite() == false) blackPieces.emplace_back(square->getPiece()); 
+
+        allPieces[WHITE] = whitePieces; 
+        allPieces[BLACK] = blackPieces; 
+
+    }
 }
 
--- board.h
+++ board.h
@@ -4,13 +4,18 @@
 #include "piece.h"
 #include "square.h"
 #include <vector>
+#include <map>
+#include <string>
 
 class Board {
-        std::vector<Piece> allPieces; 
+        enum Color {WHITE, BLACK}; 
+        std::map<Color, std::vector<Piece>> allPieces; 
         std::vector<Square*> board;
         int xDimension = 8;
         int yDimension = 8;
+        void updateAllPieces(); 
     public:
+        Board(); 
         Square* getSquare(const int x, const int y) const;
         void setSquare(const int x, const int y, Square* square); 
         void movePiece(const int fromX, const int fromY, const int toX, const int toY);
