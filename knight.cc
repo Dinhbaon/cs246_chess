14c5428b3797e8f7da1b63d66a6ed6e1dacdae30
Dinhbaon <dinhbao142@gmail.com>
2024-07-19 03:51:25 -0400

    fixed knight movement

--- knight.cc
+++ knight.cc
@@ -7,10 +7,9 @@
 Knight::Knight(Color color) : Piece(color) {}
 
 bool Knight::canMove(const Move& move, const Board& board) const {
-    // If not on diagonal return false
     int dx = move.end.getX() - move.start.getX();
     int dy = move.end.getY() - move.start.getY(); 
-    if (!(abs(dx) == 2 && abs(dy) == 1) || (abs(dy) == 2 && abs(dx) == 1)) return false; 
+    if (!((abs(dx) == 2 && abs(dy) == 1) || (abs(dy) == 2 && abs(dx) == 1))) return false; 
 
     // Check if final square is friendly piece
     Square* endSquare = board.getSquare(move.end.getX(), move.end.getY());
