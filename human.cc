9ca37a5523cbcde7230c9085f730a37062709378
Dinhbaon <dinhbao142@gmail.com>
2024-07-20 00:53:59 -0400

    added handlePromotion to human

--- human.cc
+++ human.cc
@@ -1,5 +1,9 @@
 #include "human.h"
 #include "move.h"
+#include "queen.h"
+#include "knight.h"
+#include "bishop.h"
+#include "rook.h"
 #include <string>    
 #include <iostream>
 
@@ -29,6 +33,30 @@ Move Human::getNextMove() const {
 
 void Human::handlePromotion() {
     std::string newPiece; 
-    std::cout << "Choose a piece to promote your pawn to (Q(ueen), B(ishop), K(night), R(ook))" <<std::endl; 
+    std::cout << "Choose a piece to promote your pawn to (Q(ueen), B(ishop), K(night), R(ook))" <<std::endl;
+    Piece* piece; 
+    while (true) {
+        if (newPiece == "Q") {
+            piece = new Queen{color};
+            break; 
+        } else if (newPiece == "R") {
+            piece = new Rook{color}; 
+            break; 
+        } else if (newPiece == "B") {
+            piece = new Bishop{color};
+            break;  
+        } else if (newPiece == "K") {
+            piece = new Knight{color}; 
+            break; 
+        } else {
+            std::cout << "Invalid selection" << std::endl; 
+            continue; 
+        }
+    } 
     std::cin >> newPiece; 
+
+
+    Move lastMove = board->getLastMove(); 
+
+    board->setSquare(lastMove.end.getX(), lastMove.end.getY(), new Square{lastMove.end.getX(), lastMove.end.getY(), piece}); 
 }
---
+++ piecetype.h
@@ -0,0 +1,6 @@
+#ifndef PIECETYPE_H
+#define PIECETYPE_H
+
+enum PieceType{PAWN, QUEEN, KING, ROOK, BISHOP, KNIGHT}; 
+
+#endif
