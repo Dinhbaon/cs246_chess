ae741b24ad571bdaab44258f560769628f50e91d
Dinhbaon <dinhbao142@gmail.com>
2024-07-19 22:19:39 -0400

    implemented queen

--- queen.cc
+++ queen.cc
@@ -1,7 +1,11 @@
 #include "queen.h"
+#include "bishop.h"
+#include "rook.h"
 
 Queen::Queen(Color color): Piece{color} {}
 
 bool Queen::canMove(const Move& move, const Board& board) const {
-    return true;
+    Bishop tempBishop{this->getColor()}; 
+    Rook tempRook{this->getColor()}; 
+    return tempBishop.canMove(move, board) || tempRook.canMove(move, board);
 }
