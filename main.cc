b25260d7127d8cf71935de71388c158eb20299a7
Dinhbaon <dinhbao142@gmail.com>
2024-07-22 15:20:38 -0400

    added additional check for bot moves

--- main.cc
+++ main.cc
@@ -58,6 +58,10 @@ int main() {
 
                 } else {
                     move = controller.getPlayerTurn()->getNextMove(); 
+
+                    while (!controller.isValidMove(move, controller.getPlayerColor())) {
+                        move = controller.getPlayerTurn()->getNextMove(); 
+                    }
                 }
 
 
