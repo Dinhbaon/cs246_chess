86c5e2a8e0b67a0f0a6bfe0a575c0592bfb49d4c
nqbui <nqbui@uwaterloo.ca>
2024-07-21 23:32:21 -0400

    botLevel2 and botLevel3

---
+++ botLevel3.cc
@@ -0,0 +1,31 @@
+#include "botLevel3.h"
+#include "king.h"
+#include <random>
+
+template <typename T> T getRandom(std::vector<T> vector) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrMoves(0, vector.size());
+    int ithElement = distrMoves(gen);
+    return vector[ithElement];
+}
+
+BotLevel3::BotLevel3(Color color, Board* board): Bot{color, board}{}
+
+Move BotLevel3::getNextMove() const {
+    std::vector<Square*> &squares = board->getAllSquaresWithPieces()[color];
+    std::vector<Move> captureMoves = findCaptureMoves(squares, board, color);
+    std::vector<Move> checkMoves = findCheckMoves(squares, board, color);
+    std::vector<Move> getCaptureMoves = findGetCaptureMoves(squares, board, color);
+    std::vector<Move> getRandomMoves = findRandomMoves(squares, board, color);
+
+    if(captureMoves.size() > 0){
+        return getRandom<Move>(captureMoves);
+    } else if(checkMoves.size() > 0){
+        return getRandom<Move>(checkMoves);
+    } else if (getCaptureMoves.size() > 0){
+        return getRandom<Move>(getCaptureMoves);
+    } else {
+        return getRandom<Move>(getRandomMoves);
+    }
+}
