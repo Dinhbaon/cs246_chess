feb9e4ffae80e7af929280bd4c1edf63c2235625
arkempto <arkempto@uwaterloo.ca>
2024-07-21 22:51:46 -0400

    made setup functioning

--- board.h
+++ board.h
@@ -35,7 +35,7 @@ class Board {
         bool isPiecePinned(const Square& square, Color color);  
         // Getters and setters
         Square* getSquare(const int x, const int y) const;
-        void setSquare(const int x, const int y, Piece* square); 
+        void setSquare(const int x, const int y, Piece* piece); 
         std::map<Color, std::vector<Piece*>> getPieces() const;
         Move getLastMove() const;
         Square* getKingSquare(Color color) const;
