c83b7d1c04995267ab3a8b4a0a19f21b51c0faf7
arkempto <arkempto@uwaterloo.ca>
2024-07-23 19:05:20 -0400

    took off error messages from setup fixing

--- main.cc
+++ main.cc
@@ -20,7 +20,7 @@ int main() {
     std::string command;
     std::vector<Observer*> observers;
     observers.emplace_back(new Text{&controller});
-    observers.emplace_back(new Graphic{&controller});
+    //observers.emplace_back(new Graphic{&controller});
     controller.printInit();
 
 
@@ -77,79 +77,92 @@ int main() {
         } else if (command == "undo") {
 
         } else if (command == "setup") {
+            if (controller.getMode() == GAME) {
+                std::cout << "Can't enter setup mode when in game mode." << std::endl;
+                continue;
+            }
+
+            std::cout << "setup command" << std::endl;
             controller.setMode(SETUP); 
             while (std::cin >> command) {
                 Color c = BLACK;
                 PieceType pt = ROOK;
                 std::string s;
                 if (command == "done") {
-                    if (board->isInCheck(WHITE) || board->isInCheck(BLACK)) {
-                        std::cout << "A Knight is in check - cannot exit setup \
-                                      mode until both Knights are not in check." << std::endl;
-                    } else if (!(board->oneKing(WHITE) && board->oneKing(BLACK))) {
+                    if (!(board->oneKing(WHITE) && board->oneKing(BLACK))) {
                         std::cout << "There are two Kings of the same color on the board \
                                       - cannot exit setup mode until one King of each color \
                                       are on the board." << std::endl;
+                    } else if (board->isInCheck(WHITE) || board->isInCheck(BLACK)) {
+                        std::cout << "A Knight is in check - cannot exit setup \
+                                      mode until both Knights are not in check." << std::endl; 
                     } else if (controller.checkPromotion()) {
                         std::cout << "There is a pawn in the first or last rows of the board \
                                       - cannot exit setup mode until there are no pawns in \
                                       either the first or last rows of the board." << std::endl;
                     } else {
                         controller.setMode(START);
+                        controller.printInit();
                         break;
                     }
                 } else if (command == "+") {
-                    if (command == "R") {
-                        c = WHITE;
-                        pt = ROOK;
-                    } else if (command == "N") {
-                        c = WHITE;
-                        pt = KNIGHT;
-                    } else if (command == "B") {
-                        c = WHITE;
-                        pt = BISHOP;
-                    } else if (command == "Q") {
-                        c = WHITE;
-                        pt = QUEEN;
-                    } else if (command == "K") {
-                        c = WHITE;
-                        pt = KING;
-                    } else if (command == "P") {
-                        c = WHITE;
-                        pt = PAWN;
-                    } else if (command == "r") {
-                        pt = ROOK;
-                    } else if (command == "n") {
-                        pt = KNIGHT;
-                    } else if (command == "b") {
-                        pt = BISHOP;
-                    } else if (command =="q") {
-                        pt = QUEEN;
-                    } else if (command == "k") {
-                        pt = KING;
-                    } else if (command == "p") {
-                        pt = PAWN;
-                    }
-                    std::string src;
-                    std::cin >> src; 
+                    if (std::cin >> command) {
+                        if (command == "R") {
+                            c = WHITE;
+                            pt = ROOK;
+                        } else if (command == "N") {
+                            c = WHITE;
+                            pt = KNIGHT;
+                        } else if (command == "B") {
+                            c = WHITE;
+                            pt = BISHOP;
+                        } else if (command == "Q") {
+                            c = WHITE;
+                            pt = QUEEN;
+                        } else if (command == "K") {
+                            c = WHITE;
+                            pt = KING;
+                        } else if (command == "P") {
+                            c = WHITE;
+                            pt = PAWN;
+                        } else if (command == "r") {
+                            pt = ROOK;
+                        } else if (command == "n") {
+                            pt = KNIGHT;
+                        } else if (command == "b") {
+                            pt = BISHOP;
+                        } else if (command =="q") {
+                            pt = QUEEN;
+                        } else if (command == "k") {
+                            pt = KING;
+                        } else if (command == "p") {
+                            pt = PAWN;
+                        }
+                        std::string src;
+                        std::cin >> src; 
 
-                    int xIndex = src[0] - 'a'; 
-                    int yIndex = src[1] - '0' - 1; 
-                    if (!((board->getSquare(xIndex, yIndex))->isEmpty())) {
-                        delete (board->getSquare(xIndex, yIndex))->getPiece();
-                    }
-                    if (pt == ROOK) {
-                        board->setSquare(xIndex, yIndex, new Rook(c));
-                    } else if (pt == KNIGHT) {
-                        board->setSquare(xIndex, yIndex, new Knight(c));
-                    } else if (pt == BISHOP) {
-                        board->setSquare(xIndex, yIndex, new Bishop(c));
-                    } else if (pt == QUEEN) {
-                        board->setSquare(xIndex, yIndex, new Queen(c));
-                    } else if (pt == KING) {
-                        board->setSquare(xIndex, yIndex, new King(c));
-                    } else {
-                        board->setSquare(xIndex, yIndex, new Pawn(c));
+                        int xIndex = src[0] - 'a'; 
+                        int yIndex = src[1] - '0' - 1; 
+
+                        if (!((board->getSquare(xIndex, yIndex))->isEmpty())) {
+                            delete (board->getSquare(xIndex, yIndex))->getPiece();
+                            board->getSquare(xIndex, yIndex)->setPiece(nullptr);
+                        }
+        
+                        if (pt == ROOK) {
+                            board->setSquare(xIndex, yIndex, new Rook(c));
+                        } else if (pt == KNIGHT) {
+                            board->setSquare(xIndex, yIndex, new Knight(c));
+                        } else if (pt == BISHOP) {
+                            board->setSquare(xIndex, yIndex, new Bishop(c));
+                        } else if (pt == QUEEN) {
+                            board->setSquare(xIndex, yIndex, new Queen(c));
+                        } else if (pt == KING) {
+                            board->setSquare(xIndex, yIndex, new King(c));
+                        } else {
+                            board->setSquare(xIndex, yIndex, new Pawn(c));
+                        }
+                        
                     }
                 } else if (command == "-") {
                     std::string src;
@@ -157,7 +170,9 @@ int main() {
 
                     int xIndex = src[0] - 'a'; 
                     int yIndex = src[1] - '0' - 1; 
+
                     delete (board->getSquare(xIndex, yIndex))->getPiece();
+                    board->getSquare(xIndex, yIndex)->setPiece(nullptr);
 
                 } else { // command == "="
                     std::cin >> s;
